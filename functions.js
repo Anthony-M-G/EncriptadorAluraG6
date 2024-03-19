ff3825a209b7e5b6edb64887b6f87e4c83f58b83
Anthony <antony.medinagarcia24@gmail.com>
2024-03-18 23:02:01 -0600

    aprendiendo mas sobre git

--- functions.js
+++ functions.js
@@ -82,7 +82,7 @@ let parrafo=llamadaElementoID("parrafo")
 let abcdario=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","u","v","w","x","y","z"];
 
 
-
+// comment sin sentido
 
 
 
